3ea2dc7fd8af66cbf25a6497f82582567ff12a08
Megan Png <s10196822@connect.np.edu.sg>
2021-01-26 21:36:02 +0800

    electrical project

---
+++ Electricle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Electricle
+//
+//  Created by P. Megan on 26/1/21.
+//
+import CoreData
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
