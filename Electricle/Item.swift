527f0d15b0e38721267e7e3db648e19c346b7817
Megan Png <s10196822@connect.np.edu.sg>
2021-01-28 21:24:55 +0800

    added item data

---
+++ Electricle/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Electricle
+//
+//  Created by P. Megan on 28/1/21.
+//
+import UIKit
+import Foundation
+
+class Item{
+    var Title:String
+    var Description:String
+    init(title:String, description:String) {
+        Title = title
+        Description = description
+
+    }
+}
