1221570541434dab6f43991c23df57e22c1b1192
Megan Png <s10196822@connect.np.edu.sg>
2021-01-30 00:59:24 +0800

    code for collection view

--- Electricle/ProfileController.swift
+++ Electricle/ProfileController.swift
@@ -4,9 +4,52 @@
 //
 //  Created by P. Megan on 29/1/21.
 //
-
+import UIKit
 import Foundation
 
-class ProfileController{
+class ProfileController: UIViewController{
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 165, height: 253)
+        collectionView.collectionViewLayout = layout
+        collectionView.register(collectionViewCell.nib(), forCellWithReuseIdentifier: collectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+}
+
+extension ProfileController:UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        print("Item was selected")
+    }
+}
+
+extension ProfileController:UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewCell.identifier, for: indexPath) as! collectionViewCell
+        cell.configure(with: UIImage(named: "image")!, String, String, UIImage(named: "icon")!, String)
+        
+        return cell
+    }
+}
+
+
+extension ProfileController:UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 165, height: 253)
+    }
 }
---
+++ Electricle/collectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  collectionViewCell.swift
+//  Electricle
+//
+//  Created by P. Megan on 30/1/21.
+//
+import Foundation 
+import UIKit
+
+class collectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet weak var listingTitle: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var locationIcon: UIImageView!
+    @IBOutlet weak var locationDistance: UILabel!
+    
+    static let identifier = "collectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    public func configure(with image:UIImage, title:String, username:String, icon:UIImage, distance:String){
+        imageView.image = image
+        listingTitle.text = title
+        userName.text = username
+        locationIcon.image = icon
+        locationDistance.text = distance
+    }
+    
+    static func nib() -> UINib{
+        return UINib(nibName: "collectionViewCell", bundle: nil)
+    }
+}
