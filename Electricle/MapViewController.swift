2e1a58fd58ff151d6296bf877023222e2b7d94d4
Megan Png <s10196822@connect.np.edu.sg>
2021-01-30 21:48:08 +0800

    mapviewcontroller

---
+++ Electricle/MapViewController.swift
@@ -0,0 +1,64 @@
+//
+//  MapController.swift
+//  Electricle
+//
+//  Created by P. Megan on 30/1/21.
+//
+import CoreData
+import UIKit
+import MapKit
+import Foundation
+
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager:CLLocationManager = CLLocationManager()
+    
+    @IBAction func showLocation(_ sender: Any) {
+        print(locationManager.location!)
+        centreMapOnLocation(location: locationManager.location!.coordinate)
+        //mapView.showsUserLocation = true
+            }
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //mapView.showsUserLocation = true
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+        centreMapOnLocation(location: locationManager.location!.coordinate)
+    }
+    
+    let regionRadius:CLLocationDistance = 250
+    
+    func centreMapOnLocation(location: CLLocationCoordinate2D){
+        let coordinateRegion = MKCoordinateRegion (center:location, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        //mapView.showsUserLocation = true
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Me"
+        self.mapView.addAnnotation(annotation)
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString("535 Clementi Road Singapore 599489", completionHandler: {p,e in
+            let lat = String(format: "Lat: %3.12f", p![0].location!.coordinate.latitude)
+            let long = String(format: "Long: %3.12f", p![0].location!.coordinate.longitude)
+            let nplocation = CLLocation(latitude: p![0].location!.coordinate.latitude, longitude: p![0].location!.coordinate.longitude)
+            
+            let annotation2 = MKPointAnnotation()
+            annotation2.coordinate = nplocation.coordinate
+            annotation2.title = "Ngee Ann Polytechnic"
+            annotation2.subtitle = "School Of ICT"
+            self.mapView.addAnnotation(annotation2)
+            print("\(lat), \(long)")
+        })
+        
+        
+    }
+}
---
+++ Electricle/ShowListingController.swift
@@ -0,0 +1,19 @@
+//
+//  ShowListingController.swift
+//  Electricle
+//
+//  Created by P. Megan on 30/1/21.
+//
+
+import Foundation
+import UIKit
+
+class ShowListingController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
